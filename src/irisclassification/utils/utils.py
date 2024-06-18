f0335575f525f6be107ea59ee136940abebf23aa
Ransingh Satyajit Ray <satyajit239@gmail.com>
2024-06-19 00:17:13 +0530

    msg

--- src/irisclassification/utils/utils.py
+++ src/irisclassification/utils/utils.py
@@ -101,4 +101,4 @@ def load_object(file_path):
             return pickle.load(file_obj)
     except Exception as e:
         logging.info('Exception Occured in load_object function utils')
-        raise customexception(e,sys)
+        raise customexception(e, sys)
